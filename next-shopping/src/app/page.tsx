4663c31e48ddade49b02b4c540c1f33ea1196cfc
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-08-29 20:46:02 +0200

    header component, cant fetch data currently

--- next-shopping/src/app/page.tsx
+++ next-shopping/src/app/page.tsx
@@ -1,7 +1,33 @@
 import Image from 'next/image'
+import RootLayout from './layout'
+import Header from '@/components/header'
 
-export default function Home() {
+export default function Home({categories}) {
+
+  type Categories = {
+  id: string,
+  name: string,
+  image: string
+}
+async function getAllCategories() {
+  const res = await fetch('https://api.escuelajs.co/api/v1/categories');
+  console.log(res)
+  return res.json();
+
+}
+async function getStaticProps() {
+  const categories : Categories[] = await getAllCategories()
+  return {
+      props: {
+        categories,
+      },
+    };
+}
   return (
-    <main></main>
+    <RootLayout>
+    <main>
+      <Header categories={categories}/>
+    </main>
+    </RootLayout>
   )
 }

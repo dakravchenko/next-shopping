e695f445aed3b2328c0d117574da50c3e00c2957
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-08-31 18:52:27 +0200

    js instead ts, made header client component

---
+++ next-shopping/src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import RootLayout from './layout'
+import Header from '@/components/header'
+
+export default function Home() {
+
+  return (
+    <RootLayout>
+        hey
+    </RootLayout>
+  )
+}

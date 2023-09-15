3612460f4e33339697dd61930c33534f43348aef
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-09-15 16:40:25 +0200

    add product card style, change style of add to cart comp depending on a page

--- next-shopping/app/components/AddToCart.jsx
+++ next-shopping/app/components/AddToCart.jsx
@@ -2,15 +2,20 @@
 import React from 'react'
 import { useCart } from '../contexts/CartContext'
 
-export default function AddToCart({product}) {
+export default function AddToCart({product, isProductCard}) {
   const { addToCart } = useCart();
 
   const handleAddToCart = () => {
     addToCart(product);
 
   };
+
+  const style = isProductCard
+  ? "bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+  : "absolute top-72 left-20 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded";
+
   return (
-    <button className='add-to-cart-btn' onClick={handleAddToCart}>
+    <button className={style} onClick={handleAddToCart}>
       Add to cart
     </button>
   )
--- next-shopping/app/products/[id]/page.jsx
+++ next-shopping/app/products/[id]/page.jsx
@@ -1,3 +1,4 @@
+import AddToCart from "@/app/components/AddToCart"
 import Image from "next/image"
 export async function generateStaticParams(){
     const res = await fetch('https://fakestoreapi.com/products')
@@ -16,17 +17,18 @@ async function getProductById(id){
 }
 
 export default async function ProductCard({params}) {
+  const isProductCard = true;
   const product = await getProductById(params.id)
   return (
-    <main>
-      <div className="product-info">
-        <h2 className="product-title">{product.title}</h2>
-        <p>{product.description}</p>
+    <main className="flex p-4 space-x-4">
+      <div className="flex-shrink-0">
+        <Image src={product.image} alt={product.title} width={500} height={500} />
       </div>
-      <div className="product-picture-and-add-to-cart">
-        <Image src={product.image} alt={product.title} width={100} height={100}/>
-        <p>{product.price}</p>
-        <p>Add to cart component somewhere here</p>
+      <div className="flex-1">
+        <h2 className="text-2xl font-semibold">{product.title}</h2>
+        <p className="text-gray-600">${product.price}</p>
+        <AddToCart product={product} isProductCard={isProductCard} />
+        <p className="mt-4">{product.description}</p>
       </div>
     </main>
   )

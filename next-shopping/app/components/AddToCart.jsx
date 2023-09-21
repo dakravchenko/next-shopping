e6ff5e8b02f0adfe29c0e653fe36c01cd5603725
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-09-21 15:23:13 +0200

    add styly to addtocart component when clicked

--- next-shopping/app/components/AddToCart.jsx
+++ next-shopping/app/components/AddToCart.jsx
@@ -1,22 +1,21 @@
 'use client'
-import React from 'react'
-import { useCart } from '../contexts/CartContext'
+import React from 'react';
+import { useCart } from '../contexts/CartContext';
 
-export default function AddToCart({product, isProductCard}) {
+export default function AddToCart({ product, isProductCard }) {
   const { addToCart } = useCart();
 
   const handleAddToCart = () => {
     addToCart(product);
-
   };
 
   const style = isProductCard
-  ? "bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-  : "absolute top-72 left-20 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded";
+    ? 'bg-gray-400 hover:bg-gray-500 focus:bg-gray-700 text-white font-bold py-2 px-4 rounded transition'
+    : 'absolute top-72 left-20 bg-gray-400 hover:bg-gray-500 focus:bg-gray-700 text-white font-bold py-2 px-4 rounded transition';
 
   return (
     <button className={style} onClick={handleAddToCart}>
       Add to cart
     </button>
-  )
+  );
 }
